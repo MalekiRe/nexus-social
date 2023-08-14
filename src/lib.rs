ef838038746e6e7c7dc82be0db0d489472106ee7
MalekiRe <pocmalek@gmail.com>
2023-08-14 16:21:45 -0700

    more api surface

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,81 @@
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
+use crate::changeable::{FriendRequest, User};
+use anyhow::Result;
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Debug, Serialize, Deserialize, Default)]
-pub struct Url(pub String);
+pub mod changeable {
+    use serde::{Deserialize, Serialize};
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Debug, Serialize, Deserialize, Default)]
-#[repr(C)]
-pub struct Username{ pub username: String, pub website: String}
-impl AsRef<Username> for Username {
-    fn as_ref(&self) -> &Username {
-        &self
-    }
+    #[derive(Clone, Debug, Serialize, Deserialize)]
+    pub struct User(pub String);
+
+    #[derive(Clone, Debug, Serialize, Deserialize)]
+    pub struct FriendRequest(pub String);
 }
-impl Username {
-    pub fn from(string: impl AsRef<str>) -> Self {
-        let string = string.as_ref().to_string();
-        Self {
-            username: string.split_once('.').unwrap().0.to_string(),
-            website: string.split_once('.').unwrap().1.to_string(),
-        }
-    }
-    pub fn to_url(&self) -> Url {
-        Url(String::from("http://") + &self.website + "/" + &self.username)
-    }
-}
-#[derive(Clone, Debug, Serialize, Deserialize, Default, Eq, PartialEq)]
+
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Url(String);
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Invite {
-    pub from: Username,
-    pub to: Username,
-    pub uuid: InviteUuid,
+    uuid: String,
+    game: GameInfo,
 }
-#[derive(Clone, Debug, Serialize, Deserialize, Default, Eq, PartialEq)]
-pub struct FriendRequest {
-    pub from: Username,
-    pub to: Username,
-    pub uuid: FriendRequestUuid,
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Game {
+    id: String,
+    publish_server: Url,
 }
-#[derive(Clone, Debug, Serialize, Deserialize, Default, Eq, PartialEq)]
-pub struct UnfriendRequest {
-    pub from: Username,
-    pub to: Username,
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct GameInfo {
+    game: Game,
+    instance: String,
+    join_info: String,
+    players: Option<Vec<User>>,
 }
-#[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct AvatarMeta {
-    format: AvatarFormat,
-    link: Url,
-    uuid: AvatarUuid,
+
+#[async_trait]
+trait UserApi {
+    async fn get_username(user: &User) -> Result<String>;
+    #[deprecated]
+    async fn create_user(username: String) -> Result<User>;
 }
-#[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub enum AvatarFormat {
-    #[default]
-    Vrm1_0,
-    ReadyPlayerMe,
+
+#[async_trait]
+trait FriendApi {
+    async fn send_friend_request(from: &mut User, to: &User) -> Result<()>;
+    async fn get_received_friend_requests(user: &User) -> Result<Vec<FriendRequest>>;
+    async fn get_sent_friend_requests(user: &User) -> Result<Vec<FriendRequest>>;
+    async fn accept_friend_request(user: &mut User, friend_request: &FriendRequest) -> Result<()>;
+    async fn deny_friend_request(user: &mut User, friend_request: &FriendRequest) -> Result<()>;
 }
-#[derive(Eq, PartialEq, Hash, Clone, Debug, Serialize, Deserialize, Default)]
-pub struct InviteUuid(pub String);
-#[derive(Eq, PartialEq, Hash, Clone, Debug, Serialize, Deserialize, Default)]
-pub struct FriendRequestUuid(pub String);
-#[derive(Eq, PartialEq, Hash, Clone, Debug, Serialize, Deserialize, Default)]
-pub struct AvatarUuid(pub String);
+
+#[async_trait]
+trait BioApi {
+    async fn set_bio(user: &mut User, bio: String) -> Result<()>;
+    async fn get_bio(user: &User) -> Result<String>;
+}
+
+#[async_trait]
+trait InviteApi {
+    async fn send_invite(from: &mut User, to: &User, invite: Invite) -> Result<()>;
+    async fn received_invites(user: &User) -> Result<Vec<Invite>>;
+    async fn sent_invites(user: &User) -> Result<Vec<Invite>>;
+    async fn remove_invite(user: &mut User, invite: &Invite) -> Result<()>;
+}
+
+#[async_trait]
+trait GameApi {
+    async fn set_current_game(user: &mut User, game: GameInfo) -> Result<()>;
+    async fn get_current_game(user: &User) -> Result<Option<GameInfo>>;
+    async fn make_game_public(game_info: &GameInfo);
+    async fn get_public_games(game: Game) -> Result<Vec<GameInfo>>;
+}
+
+#[async_trait]
+trait GameApiExtensions {
+    async fn get_hot_games(game: Game) -> Result<Vec<GameInfo>>;
+    async fn get_games_type(game: Game, game_type: String) -> Result<Vec<GameInfo>>;
+}
